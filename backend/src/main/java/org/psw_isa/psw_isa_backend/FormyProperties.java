edede2f6d9e661bb02beb9155a11795b47e68850
Stefan Nožinić <stefan@lugons.org>
2024-01-28 16:03:40 +0100

    Update FormyProperties configuration

--- backend/src/main/java/org/psw_isa/psw_isa_backend/FormyProperties.java
+++ backend/src/main/java/org/psw_isa/psw_isa_backend/FormyProperties.java
@@ -1,11 +1,13 @@
 package org.psw_isa.psw_isa_backend;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConfigurationProperties(prefix = "formy")
+@ConfigurationProperties
 public class FormyProperties {
+    @Value("${formy.only-authenticated-users-can-upload-images:true}")
     private boolean onlyAuthenticatedUsersCanUploadImages;
 
     public boolean getOnlyAuthenticatedUsersCanUploadImages() {
