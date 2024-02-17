bfe01cf10e1b9f5a92e5cf984d78d8de60cb01fa
Stefan Nožinić <stefan@lugons.org>
2024-02-17 19:18:03 +0100

    Update dependencies and fix input placeholders

--- frontend/src/widgets/User/WidgetUserSingleEdit.vue
+++ frontend/src/widgets/User/WidgetUserSingleEdit.vue
@@ -20,23 +20,23 @@ export default {
     <div class="widget-user-single-edit"> 
     <div> 
 		<p>
-		<input type="text" class="form-control" placeholder={{user.name}} v-model="data.name"  />
+		<input type="text" class="form-control" v-bind:placeholder="user.name" v-model="data.name"  />
 		</p>
 		
         <p>
-		<input type="text" class="form-control" placeholder={{user.lastname}} v-model="data.lastname" />
+		<input type="text" class="form-control" v-bind:placeholder="user.lastname" v-model="data.lastname" />
 		</p>
 
         <p>
-		<input type="date" class="form-control" placeholder={{user.birthday}} v-model="data.birthday" />
+		<input type="date" class="form-control" v-bind:placeholder="user.birthday" v-model="data.birthday" />
 		</p>
 
 		<p>
-		<input type="text" class="form-control" placeholder={{user.address}} v-model="data.address" />
+		<input type="text" class="form-control" v-bind:placeholder="user.address" v-model="data.address" />
 		</p>
 
         <p>
-		<input type="number" class="form-control" placeholder={{user.mobile_phone}} v-model="data.mobile_phone" />
+		<input type="number" class="form-control" v-bind:placeholder="user.mobile_phone" v-model="data.mobile_phone" />
 		</p>
 		
 		<button type="button" class="btn btn-primary btn-lg btn-block" v-on:click="submit">Submit</button>
