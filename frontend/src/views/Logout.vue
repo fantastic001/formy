2031bc8a171d489e843a0677a09ac26e2087e62c
Stefan Nožinić <stefan@lugons.org>
2024-06-21 13:31:51 +0200

    Update Logout.vue to clear user data and redirect to home page

--- frontend/src/views/Logout.vue
+++ frontend/src/views/Logout.vue
@@ -18,6 +18,9 @@ export default {
             this.$router.push("/");
         }).catch(error => {
             console.log(error);
+            localStorage.setItem("user", "");
+            this.$store.commit("login", {user: "", role: "NOT_LOGGED"});
+            this.$router.push("/");
         });
         
     },
