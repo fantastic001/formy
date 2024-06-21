6dbe84e1aea6b0e59415acbf89ef72bdb71a48ae
Stefan Nožinić <stefan@lugons.org>
2024-06-21 12:30:08 +0200

    Add logout functionality using axios

--- frontend/src/views/Logout.vue
+++ frontend/src/views/Logout.vue
@@ -1,5 +1,7 @@
 <script>
 
+import axios from "axios";
+import { API_URL } from "../config"
 
 export default {
     name: "LogOut",
@@ -10,9 +12,14 @@ export default {
 	},
     mounted: function () 
     {
-	localStorage.setItem("user", "");
-	this.$store.commit("login", {user: "", role: "NOT_LOGGED"});
-	this.$router.push("/");
+        axios.delete(API_URL + "/login").then(response => {
+            localStorage.setItem("user", "");
+            this.$store.commit("login", {user: "", role: "NOT_LOGGED"});
+            this.$router.push("/");
+        }).catch(error => {
+            console.log(error);
+        });
+        
     },
 }
 </script>
